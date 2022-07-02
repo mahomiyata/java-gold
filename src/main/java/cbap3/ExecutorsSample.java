623a265e30cabee1c4f85000d3e05b4dd65f1ffa
Maho Miyata <mahomiyata@users.noreply.github.com>
2022-07-02 17:48:55 +0900

    Executors サンプル

---
+++ src/main/java/cbap3/ExecutorsSample.java
@@ -0,0 +1,33 @@
+package cbap3;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorsSample {
+    public static void main(String[] args) throws InterruptedException {
+        Runnable test = () -> System.out.println(Thread.currentThread().getId());
+
+        ExecutorService singleExec = Executors.newSingleThreadExecutor();
+        for (int i = 0; i < 5; i++) {
+            singleExec.submit(test);
+        }
+        singleExec.shutdown();
+        System.out.println("singleExec status: is shutdown?" + singleExec.isShutdown());
+
+        ExecutorService cachedExec = Executors.newCachedThreadPool();
+        for (int i = 0; i < 5; i++) {
+            cachedExec.submit(test);
+        }
+        Thread.sleep(10000);
+        System.out.println("---------10秒後---------");
+        for (int i = 0; i < 5; i++) {
+            cachedExec.submit(test);
+        }
+        Thread.sleep(70000);
+        System.out.println("---------70秒後---------");
+        for (int i = 0; i < 5; i++) {
+            cachedExec.submit(test);
+        }
+        cachedExec.shutdown();
+    }
+}
