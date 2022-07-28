5ceeb3b5eb2bb19e56300420200a5a30c1316f12
Maho Miyata <mahomiyata@users.noreply.github.com>
2022-07-29 00:52:49 +0900

    Reduce サンプル

---
+++ src/main/java/cbap3/ReduceSample.java
@@ -0,0 +1,22 @@
+package cbap3;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+public class ReduceSample {
+    public static void main(String[] args) {
+        List<LinkedHashMap<String, Object>> chars = new ArrayList<>();
+        LinkedHashMap<String,Object> a = new LinkedHashMap<>();
+        a.put("greeting", "Hello");
+        LinkedHashMap<String,Object> b = new LinkedHashMap<>();
+        b.put("greeting", "Konnichiha");
+        LinkedHashMap<String,Object> c = new LinkedHashMap<>();
+        c.put("greeting", "Hello");
+//        chars.add(a);
+//        chars.add(b);
+//        chars.add(c);
+        String concat = (String) chars.stream().map(item -> item.get("greeting")).reduce("", (accum, value) -> accum + value.toString());
+        System.out.println(concat);
+    }
+}
